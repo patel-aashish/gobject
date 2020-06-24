1e7cc0f9b7db0231cec4ef5224daa6bdb9b7a7a0
Aashish Patel <aashish.a.patel@gmail.com>
2020-06-24 13:33:15 +0530

    initial commit

---
+++ example-person.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 Aashish Patel
+// 
+// This file is part of example-person. The code is written following
+// https://www.youtube.com/watch?v=1-_EBEr0fxI
+// 
+// example-person is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// example-person is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with example-person.  If not, see <http://www.gnu.org/licenses/>.
